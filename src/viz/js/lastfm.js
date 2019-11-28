4f8725af230d84e6b485d3b7cec502156a8f6720
Ondrej Zacha <ozacha@xccelerated.io>
2019-11-28 17:19:58 +0100

    Add working transitions between months; change input data structure

--- src/viz/js/lastfm.js
+++ src/viz/js/lastfm.js
@@ -1,7 +1,7 @@
 function show() {
     'use strict';
 
-    let margin = { top: 40, bottom: 40, right: 40, left: 40 },
+    let margin = { top: 140, bottom: 40, right: 40, left: 140 },
         width = 800 - margin.left - margin.right,
         height = 600 - margin.top - margin.bottom;
 
@@ -9,77 +9,58 @@ function show() {
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
 
-
     // chart configuration
-    const namesToShow = 10;
-    const barWidth = 20;
-    const barMargin = 5;
-    const maxRadius = 100;
+    const maxRadius = 200;
 
-    // First we're going to load the data
-    d3.json(
-//    '../../data/sample_counts.csv',
-    '../../notebooks/json2.json',
-//    function (d) { return { tag: d.tag, ts: d.ts, tag_count: +d.tag_count }; },
-    function (data) {
-
-        // Step 0. If you're interested that is. Use console.log to output the data
-        console.log(data);
-
-
+    d3.json('../../notebooks/json7.json', function (data) {
 
         chart.on('click', onclick);
-        var currentMonth = '2011-03-31'
+        var currentMonth = '2011-03-31';
+        let months = [
+            "2011-03-31", "2011-06-30", "2011-09-30", "2011-12-31", "2012-03-31", "2012-06-30",
+            "2012-12-31", "2013-03-31", "2013-06-30", "2013-09-30", "2013-12-31"
+        ]
 
-        function onclick(d){
+        function onclick(){
             console.log('before', currentMonth)
-            console.log('clicked')
-            currentMonth = '2011-06-30'
-            console.log(currentMonth)
-            showBubbles(currentMonth)
+            currentMonth = months.shift();
+            chart.selectAll('circle')
+                .transition().duration(1000)
+                .attr("r", d => Math.sqrt(+d[currentMonth].tag_count) * 3)
         }
 
+        initializeBubbles(currentMonth)
 
-        showBubbles(currentMonth)
+        function initializeBubbles(month) {
+            console.log('running initializeBubbles')
 
-        function showBubbles(month) {
-            let data_month = data[month];
-            console.log(data_month);
+//            const maxCount = 10; //d3.max(data_month, d => Math.sqrt(d.tag_count));
+//
+//            let bubbleRadius = d3.scaleLinear()
+//              .domain([0, maxCount])
+//              .range([0, maxRadius]);
 
-            // We've already created the SVG groups and positioned them
-        // using the transform property.
-        let bubbles = chart.selectAll('g').data(data_month)
-          .enter().append('g')
-            .attr("transform", (d, i) => "translate(" +
-                (margin.left + i / 11 * width) + "," +
-                (margin.top + i / 11 * height) + ")"
-            );
+            // Initialize all bubble groups
+            let bubbles = chart.selectAll('g').data(data)
+              .enter().append('g')
+                .attr("transform", (d, i) => "translate(" +
+                    (margin.left + i / 11 * width) + "," +
+                    (margin.top + i / 11 * height) + ")"
+                );
 
-        // Step 1. Setup a d3.scaleLinear that handles the width of the bars.
-        const maxCount = d3.max(data_month, d => Math.sqrt(d.tag_count));
+            bubbles
+              .append('circle')
+                .attr('r', 0)
+                .transition().duration(1000)
+                .attr("r", d => Math.sqrt(+d[month].tag_count) * 3)
+                .attr("class", 'bubble');
 
-        let bubbleRadius = d3.scaleLinear()
-          .domain([0, maxCount])
-          .range([0, maxRadius]);
+            bubbles
+              .append('text')
+                .text(d => d.tag)
+                .attr('class', 'label');
 
-        bubbles
-          .append('circle')
-            .attr('r', 10)
-            .transition().duration(1000)
-            .attr("r", d => bubbleRadius(Math.sqrt(d.tag_count)))
-            .attr("class", 'bubble');
-
-        bubbles
-          .append('text')
-            .text(d => d.tag)
-            .attr('class', 'label');
-
-        chart.selectAll('g').transition().duration(5000).remove()
         }
-
-
-
-
 
     });
 }
