13e1cdbe2e852858550535a15dcfc34092cfd795
Ondrej Zacha <ozacha@xccelerated.io>
2019-11-29 09:48:49 +0100

    Change input data structure again

--- src/viz/js/lastfm.js
+++ src/viz/js/lastfm.js
@@ -12,13 +12,17 @@ function show() {
     // chart configuration
     const maxRadius = 200;
 
-    d3.json('../../notebooks/json7.json', function (data) {
+    d3.json('../../notebooks/json8.json', function (data) {
 
         chart.on('click', onclick);
         var currentMonth = '2011-03-31';
         let months = [
-            "2011-03-31", "2011-06-30", "2011-09-30", "2011-12-31", "2012-03-31", "2012-06-30",
-            "2012-12-31", "2013-03-31", "2013-06-30", "2013-09-30", "2013-12-31"
+            '2011-03-31', '2011-06-30', '2011-09-30', '2011-12-31', '2012-03-31', '2012-06-30', '2012-09-30',
+            '2012-12-31', '2013-03-31', '2013-06-30', '2013-09-30', '2013-12-31', '2014-03-31', '2014-06-30',
+            '2014-09-30', '2014-12-31', '2015-03-31', '2015-06-30', '2015-09-30', '2015-12-31', '2016-03-31',
+            '2016-06-30', '2016-09-30', '2016-12-31', '2017-03-31', '2017-06-30', '2017-09-30', '2017-12-31',
+            '2018-03-31', '2018-06-30', '2018-09-30', '2018-12-31', '2019-03-31', '2019-06-30', '2019-09-30',
+            '2019-12-31'
         ]
 
         function onclick(){
@@ -26,7 +30,7 @@ function show() {
             currentMonth = months.shift();
             chart.selectAll('circle')
                 .transition().duration(1000)
-                .attr("r", d => Math.sqrt(+d[currentMonth].tag_count) * 3)
+                .attr("r", d => Math.sqrt(+d['data'][currentMonth]) * 3)
         }
 
         initializeBubbles(currentMonth)
@@ -52,7 +56,7 @@ function show() {
               .append('circle')
                 .attr('r', 0)
                 .transition().duration(1000)
-                .attr("r", d => Math.sqrt(+d[month].tag_count) * 3)
+                .attr("r", d => Math.sqrt(+d['data'][month]) * 3)
                 .attr("class", 'bubble');
 
             bubbles
